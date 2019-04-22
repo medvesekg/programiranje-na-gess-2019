2bcf0f9715db7821252484921c3f44d1c7b56d79
Gregor Medvešek <medvesek.gregor@gmail.com>
2019-04-22 10:54:35 +0200

    Koda z 8. srečanja

---
+++ 8 - Javascript DOM/functions.js
@@ -0,0 +1,11 @@
+function randint(min, max) {
+
+    let random = Math.random()
+    return Math.floor((random * (max - min + 1)) + min )
+
+}
+
+/*python
+ def functionName(param1, param2):
+    return param1 + param2
+*/
